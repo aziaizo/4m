0029fa7cef8ab3988c7289617438ba7d8c3d97a8
Aziza <118680875+aziaizo@users.noreply.github.com>
2023-02-02 20:51:33 +0600

    hw 7

--- products/views.py
+++ products/views.py
@@ -4,6 +4,8 @@ from products.forms import ProductCreateForm,ReviewCreateForm
 
 # Create your views here.
 
+PAGINATION_LIMIT=3
+
 def main(request):
     if request.method =='GET':
         return render(request, ' layouts/index.html')
@@ -16,12 +18,27 @@ def products_view(request):
             products=Product.objects.filter(category=Category.objects.get(id=category_id))
         else:
             products=Product.objects.all()
+        search=request.GET.get('search')
+        page=int(request.GET.get('page', 1))
+
+        if search is not None:
+            products=Product.objects.filter(
+                title__icontains=search
+            )
+
+        max_page=products.__len__()/PAGINATION_LIMIT
+
+        if round(max_page)<max_page:
+            max_page=round(max_page)+1
+        else:
+            max_page=round(max_page)
+
+        products=products[PAGINATION_LIMIT*(page-1):PAGINATION_LIMIT*page]
 
         context= {
             'products': products,
-            'user':request.user
-
-
+            'user':request.user,
+            'max_page':range(1,max_page+1)
         }
         return render(request, 'products/products.html', context=context)
 
